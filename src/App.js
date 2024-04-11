dacd6cfc3f5d7cf92e0a6086f58cf2e2dc89eb64
Muhammad Afzal <afzal@ignishealth.com>
2024-04-11 12:00:42 +0500

    Added lazy loafing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Header from "./components/Header";
 import RestuarantsList from "./components/RestuarantsList";
-import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestuarantMenu from "./components/RestaurantMenu";
+
+const About = lazy(() => import("./components/About"));
+const Grovery = lazy(() => import("./components/Grocery"));
 
 const AppLayout = () => {
   return (
@@ -28,11 +30,23 @@ const appRouter = createBrowserRouter([
       },
       {
         path: "/about",
-        element: <About />,
+        element: (
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <About />
+          </Suspense>
+        ),
       },
       {
         path: "/contact",
         element: <Contact />,
+      },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <Grovery />
+          </Suspense>
+        ),
       },
       {
         path: "/restaurants/:resId",
